7b0befba23d9b12a790acc5ad084a1a033ef89e6
Bihari123 <thakur.cs.tarun@gmail.com>
2022-03-31 14:38:59 +0530

    connection string

--- connection.go
+++ connection.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/snowflakedb/gosnowflake"
 )
 
 func main() {
-
+	userName := os.Getenv("SNOWFLAKES_USERNAME")
+	password := os.Getenv("SNOWFLAKES_PASSWORD")
+	database := "testdb"
+	connectionString := fmt.Sprintf("%s:%s@my_organization-my_account/%s", userName, password, database)
+	
+	db, err := sql.Open("snowflake", connectionString)
 	//db, err := sql.Open("snowflake", "jsmith:mypassword@my_organization-my_account/mydb/testschema?warehouse=mywh")   // defining the schema and warehouse
 	if err != nil {
 		log.Fatal(err)
